648d2f6cb1f60f8bc4f015732d168cb5bd9e0e53
afraz98 <anthonymfrazier1998@gmail.com>
2025-12-01 20:30:03 -0500

    Day 1 initial solution

---
+++ day01/day1.py
@@ -0,0 +1,37 @@
+import timeit
+
+def parse_input(filename):
+    return [line.strip('\n') for line in open(filename, 'r')]
+
+def solve_part_one():
+    current_position = 50
+    count = 0
+    instructions = parse_input("day1.txt")
+    for instruction in instructions:
+        direction = instruction[0]
+        movement = int(instruction[1:])
+        current_position = (current_position + movement) % 100 if direction == 'R' else (current_position - movement) % 100
+        if(current_position == 0):
+            count += 1
+    print(count)
+    pass
+
+def solve_part_two():
+    current_position = 50
+    count = 0
+    instructions = parse_input("day1.txt")
+    for instruction in instructions:
+        direction = instruction[0]
+        movement = int(instruction[1:])
+        for i in range(0, movement):
+            current_position = (current_position + 1) % 100 if direction == 'R' else (current_position - 1) % 100
+            if(current_position == 0):
+                count += 1
+    print(count)
+    pass
+
+result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
+print("Part I ran in %s seconds" % str(result))
+
+result = timeit.timeit('solve_part_two()', setup='from __main__ import solve_part_two', number=1)
+print("Part II ran in %s seconds" % str(result))
