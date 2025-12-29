679993f73986a517fbf0af921888875f9c12c4a7
afraz98 <anthonymfrazier1998@gmail.com>
2025-12-29 00:13:23 -0500

    Day 7 part two solution

--- day07/day7.py
+++ day07/day7.py
@@ -1,12 +1,15 @@
+from functools import lru_cache # Cheats enabled
 import timeit
 
 def parse_input(filename):
     return [line.strip('\n') for line in open(filename, 'r')]
 
 grid = [list(line) for line in parse_input("day7.txt")]
-visited = [[False for element in lst] for lst in grid]
 
-def traverse_path(grid, row, col):
+# Maintain list of visited nodes for part one
+visited = [[False for _ in lst] for lst in grid]
+
+def traverse_path_part_one(grid, row, col):
     if row >= len(grid):
         return 0
     if col >= len(grid[0]) or col < 0:
@@ -17,20 +20,36 @@ def traverse_path(grid, row, col):
     visited[row][col] = True
 
     if(grid[row][col] == '^'):
-        grid[row][col] = "!"
-        return 1 + traverse_path(grid, row, col+1) + traverse_path(grid, row, col-1)
+        return 1 + traverse_path_part_one(grid, row, col+1) + traverse_path_part_one(grid, row, col-1)
 
-    return traverse_path(grid, row+1, col)
+    return traverse_path_part_one(grid, row+1, col)
 
 def solve_part_one():
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == "S":
                 start_row, start_col = row, col
-    print(traverse_path(grid, start_row, start_col))
+    print(traverse_path_part_one(grid, start_row, start_col))
     pass
 
+@lru_cache
+def traverse_path_part_two(row, col):
+    if row == len(grid)-1:
+        return 1
+    if col >= len(grid[0]) or col < 0:
+        return 1
+
+    if(grid[row+1][col] == '^'):
+        return traverse_path_part_two(row, col+1) + traverse_path_part_two(row, col-1)
+
+    return traverse_path_part_two(row+1, col)
+
 def solve_part_two():
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == "S":
+                start_row, start_col = row, col
+    print(traverse_path_part_two(start_row, start_col))
     pass
 
 result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
